5d871587a7599ff1cd415ecf0f727a5a211518cd
Zach Tapia <zachtapia@gmail.com>
2020-08-12 20:07:28 -0700

    auth

---
+++ services/passport.js
@@ -0,0 +1,40 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+// User model -> cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Cookie -> User model
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // We already have a record of user, so don't add to mongo
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id }).save().then((user) => {
+            done(null, user);
+          });
+        }
+      });
+    }
+  )
+);
